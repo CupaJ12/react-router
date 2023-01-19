d31f4340a36a8358e4cb50715b220ce5b41fb4f1
CupaJ12 <108900676+CupaJ12@users.noreply.github.com>
2023-01-19 12:38:14 -0600

    .

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Link, useHistory
+} from "react-router-dom/cjs/react-router-dom.min";
 
-import Animals from '../Animals/Animals.jsx';
-import Plants from '../Plants/Plants.jsx';
-import Home from '../Home/Home.jsx';
+import "./App.css";
+
+import Animals from "../Animals/Animals.jsx";
+import Plants from "../Plants/Plants.jsx";
+import Home from "../Home/Home.jsx";
 
 function App() {
+  // const history = useHistory();
+
+  // const routeHome = () => {
+  //   alert("theres no place like home");
+  //   //here is where ill route to home
+  //   history.push('/');
+  // };
   return (
-    <div className="App">
-      <h1>SPAs!</h1>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <h1>SPAs!</h1>
+          <ul>
+            <li>
+              <Link to="/animals">Animals</Link>
+            </li>
+            <li>
+              <Link to="/plants">plants</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+          <Route path="/animals">
+            <Animals />
+          </Route>
+          <Route path="/plants">
+            <Plants />
+          </Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </div>
+      </Router>
+    </>
   );
 }
 
